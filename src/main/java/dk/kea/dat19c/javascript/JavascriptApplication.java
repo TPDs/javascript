22ec1f1cec20aeac36470354c4121c13d25b2a3f
JarlTuxen <jarl.tuxen@gmail.com>
2020-09-10 11:00:59 +0200

    first commit

---
+++ src/main/java/dk/kea/dat19c/javascript/JavascriptApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.dat19c.javascript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavascriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavascriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/dat19c/javascript/JavascriptApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.dat19c.javascript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavascriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
